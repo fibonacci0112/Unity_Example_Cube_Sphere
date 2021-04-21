e826c566b320eac79f5b75579df52df7df143b53
Nico Schiele <32066968+fibonacci0112@users.noreply.github.com>
2021-04-21 18:42:03 +0200

    Rotating Sphere and Cube added
    sphere revolves as it is a child, of a Gameobject that rotates

---
+++ Cube_Sphere/Assets/Scripts/RotatingCircle.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingCircle : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(0, 0.2f, 0); //rotates the GameObject of which Sphere is a child by 0.2 degrees on Update
+    }
+}
